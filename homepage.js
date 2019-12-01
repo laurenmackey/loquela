f34000657b11b56fb85bf2c028c12f1f4a718673
laurenmackey <lauren.m.mackey@gmail.com>
2019-11-30 20:32:30 -0800

    Fix styling on prompt display

--- homepage.js
+++ homepage.js
@@ -5,48 +5,49 @@ module.exports = function() {
   var helpers = require('./helpers');
   const session = require('express-session');
 
-function getProfileData(userId) {
-  return new Promise(function(resolve, reject) {
-    var context = {};
-
-    db.getUserById(userId).then(function(userInfo) {
-      db.getUserProfileByUserId(userId).then(function(userProfileInfo) {
-        context.language = helpers.capitalizeFirstLetter(userProfileInfo[0].language);
-        context.difficulty = helpers.capitalizeFirstLetter(userProfileInfo[0].difficulty);
-        context.topic = helpers.capitalizeFirstLetter(userProfileInfo[0].topic);
-        context.continue = userProfileInfo[0].topic;
-        context.imageUrl = helpers.getAvatarUrl(userId);
-        context.topic = userProfileInfo[0].topic;
-
-        db.getPromptsByLanguage(userProfileInfo[0].language).then(function(userPrompts) {
-          context.username = userInfo[0].username;
-          context.prompts = userPrompts;
-          resolve(context);
+  function getProfileData(userId) {
+    return new Promise(function(resolve, reject) {
+      var context = {};
+  
+      db.getUserById(userId).then(function(userInfo) {
+        db.getUserProfileByUserId(userId).then(function(userProfileInfo) {
+          context.language = helpers.capitalizeFirstLetter(userProfileInfo[0].language);
+          context.difficulty = helpers.capitalizeFirstLetter(userProfileInfo[0].difficulty);
+          context.topic = helpers.capitalizeFirstLetter(userProfileInfo[0].topic);
+          context.continue = userProfileInfo[0].topic;
+          context.imageUrl = helpers.getAvatarUrl(userId);
+          context.topic = userProfileInfo[0].topic;
+  
+          db.getPromptsByLanguage(userProfileInfo[0].language).then(function(userPrompts) {
+            context.username = userInfo[0].username;
+            context.prompts = userPrompts;
+            resolve(context);
+          });
         });
       });
     });
-  });
-}
+  }
 
-//Function to check how many prompts the user has completed in their selected topic
-function checkTopicProgress(language, topic, userId) {
-  return new Promise(function(resolve, reject) {
-    //Query DB for total number of prompts for the given user topic
-    db.getResultsByTopic(language, topic, userId).then(function(userResults) {
-      var context = {};
-      context.topicCount = 0;
-      context.topicTotal = userResults.length;
-      for(var i=0; i < userResults.length; i++) {
-        //If user has recieved a grade for a given prompt (that corresponds to a given topic)
-        //add to counter
-        if(userResults[i]["grade"] != null) {
-          context.topicCount += 1;
+  //Function to check how many prompts the user has completed in their selected topic
+  function checkTopicProgress(language, topic, userId) {
+    return new Promise(function(resolve, reject) {
+      //Query DB for total number of prompts for the given user topic
+      db.getResultsByTopic(language, topic, userId).then(function(userResults) {
+        var context = {};
+        context.topicCount = 0;
+        context.topicTotal = userResults.length;
+
+        for(var i=0; i < userResults.length; i++) {
+          //If user has recieved a grade for a given prompt (that corresponds to a given topic)
+          //add to counter
+          if(userResults[i]["grade"] != null) {
+            context.topicCount += 1;
+          }
         }
-      }
-      resolve(context);
+        resolve(context);
+      });
     });
-  });
-}
+  }
 
   router.get('/', function(req, res) {
     if(helpers.notLoggedIn(req)) {
